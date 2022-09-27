08f730799b92e7bc5aa5f1bc6fb31b01d145829a
WooHyun94 <94dngus@gmail.com>
2022-09-27 10:54:59 +0900

    docs: Studoy on Sep 27, 2022
    OpenCV

---
+++ OpenCV/ch01/HelloCV.py
@@ -0,0 +1,17 @@
+import cv2 
+import sys 
+import os
+
+print('Hello, OpenCV', cv2.__version__)
+
+img = cv2.imread('C:/Users/94dng/Documents/dev/TIL/OpenCV/ch01/cat.bmp')
+
+if img is None:
+    print('Image load failed!')
+    sys.exit()
+
+cv2.namedWindow('image')
+cv2.imshow('image', img)
+cv2.waitKey()
+
+cv2.destroyAllWindows()
