d99d884ecefdb0e8775a2a899ae7bdc8c396b97a
WooHyun94 <94dngus@gmail.com>
2022-09-27 13:56:45 +0900

    docs: Study on Sep 27, 2022

--- OpenCV/ch01/HelloCV.py
+++ OpenCV/ch01/HelloCV.py
@@ -4,13 +4,14 @@ import os
 
 print('Hello, OpenCV', cv2.__version__)
 
-img = cv2.imread('C:/Users/94dng/Documents/dev/TIL/OpenCV/ch01/cat.bmp')
+img = cv2.imread('C:/Users/94dng/Documents/dev/TIL/OpenCV/ch01/cat.bmp',flags=cv2.IMREAD_GRAYSCALE)
 
 if img is None:
     print('Image load failed!')
     sys.exit()
 
-cv2.namedWindow('image')
+#cv2.imwrite('cat_gray.png', img)
+cv2.namedWindow('image') #,cv2.WINDOW_NORMAL
 cv2.imshow('image', img)
 cv2.waitKey()
 
