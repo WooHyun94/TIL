83e5e19910ed60ca7a726a028818c7f0fbcf70cf
WooHyun94 <94dngus@gmail.com>
2023-07-10 00:18:54 +0900

    2023.07.10

--- java_/JavaWorkSpace/src/chap_01/_03_Variables.java
+++ java_/JavaWorkSpace/src/chap_01/_03_Variables.java
@@ -2,6 +2,16 @@ package chap_01;
 
 public class _03_Variables {
     public static void main(String[] args) {
-        System.out.println();
+        String name = "우현";
+        int hour = 15;
+
+        System.out.println(name + "님 배송이 시작됩니다. " + hour + "시에 방문 예정입니다.");
+        System.out.println(name + "님 배송이 완료되었습니다.");
+
+        double score = 90.5;
+        char grade = 'A';
+        name = "강백호";
+        System.out.println(name + "님의 평균 점수는 " + score + "점입니다.");
+        System.out.println(name + "님의 학점은 " + grade + "입니다.");
     }
 }
