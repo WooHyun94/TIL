d00cd91740a3a19b6610184f09779f510a85bef9
WooHyun94 <94dngus@gmail.com>
2023-07-11 23:40:52 +0900

    2023.07.11

---
+++ java_/lifeCoding/src/OkJavaGoInHomeInput.java
@@ -0,0 +1,29 @@
+import org.opentutorials.iot.Elevator;
+import org.opentutorials.iot.Security;
+import org.opentutorials.iot.Lighting;
+import javax.swing.JOptionPane;
+public class OkJavaGoInHomeInput {
+    public static void main(String[] args) {
+        String id = args[0];
+        String bright = args[1];
+
+        // Elevator call
+        Elevator myElevator = new Elevator(id);
+        myElevator.callForUp(1);
+
+        // Security off
+        Security mySecurity = new Security(id);
+        mySecurity.off();
+
+        // Light on
+        Lighting hallLamp = new Lighting(id+" / Hall Lamp");
+        hallLamp.on();
+
+        Lighting floorLamp = new Lighting(id+" / floor Lamp");
+        floorLamp.on();
+
+        DimmingLights moodLmap = new DimmingLights(id+" moodLamp");
+        moodLmap.setBright(Double.parseDouble((bright)));
+        moodLamp.on():
+    }
+}
