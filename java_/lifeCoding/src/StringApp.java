b617e0df9b34e5160451a81d9f10995392af4ce6
WooHyun94 <94dngus@gmail.com>
2023-07-11 21:25:34 +0900

    2023.07.11

---
+++ java_/lifeCoding/src/StringApp.java
@@ -0,0 +1,13 @@
+public class StringApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World"); // String
+        //System.out.println('Hello World'); // error
+        // ''(작은따옴표는 character를 나타냄
+        System.out.println("H"); // String
+
+        System.out.println("Hello " +
+                "World"); // 줄바꿈이 되지 않음
+        System.out.println("Hello \nWorld"); // 줄바꿈 n:newLine
+
+    }
+}
