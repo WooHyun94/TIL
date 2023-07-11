e8cc5a38cf41d48935d5f0b90877506677309d31
WooHyun94 <94dngus@gmail.com>
2023-07-11 21:48:58 +0900

    2023.07.11

---
+++ java_/lifeCoding/src/Letter.java
@@ -0,0 +1,10 @@
+public class Letter {
+    public static void main(String[] args) {
+        String name = "Hyun";
+        System.out.println("Hello, Hyun ... Hyun ... Hyun ... bye"); // Hyun이 반복됨
+        System.out.println("Hello, "+name+" ... "+name+" ... "+name+" ... bye");
+
+        double VAT = 10.0;
+        System.out.println(VAT);
+    }
+}
---
+++ java_/lifeCoding/src/Variable.java
@@ -0,0 +1,12 @@
+public class Variable {
+    public static void main(String[] args) {
+        int a = 1; // Number -> integer
+        System.out.println(a); // 1
+
+        double b = 1.1; // real number -> float(1.1f), double(1.1)
+        System.out.println(b); // 1.1
+
+        String c = "Hello World";
+        System.out.println(c); // Hello World
+    }
+}
