5f702da88218429be4caab1eee2ba2e26a172549
WooHyun94 <94dngus@gmail.com>
2023-07-12 21:04:11 +0900

    2023.07.12

--- java_/lifeCoding/src/AccountingClassApp.java
+++ java_/lifeCoding/src/AccountingClassApp.java
@@ -1,34 +1,70 @@
-public class AccountingClassApp {
-    public static double valueOfSupply = 10000.0;
-    public static double vatRate = 0.1;
-    public static void main(String[] args) {
-        double expenseRate = 0.3;
-        double vat = getVAT();
-        double total = valueOfSupply + vat;
-        double expense = valueOfSupply*expenseRate;
-        double income = valueOfSupply-expense;
-
-        System.out.println("Value of supply : "+valueOfSupply);
-        System.out.println("VAT : "+(vat));
-        System.out.println("Total : "+total);
-        System.out.println("Expense : "+expense);
-        System.out.println("Income : "+income);
-
-        double[] dividendRates = new double[3];
-        dividendRates[0] = 0.5;
-        dividendRates[1] = 0.3;
-        dividendRates[2] = 0.2;
-
-        double dividend1 = income * dividendRates[0];
-        double dividend2 = income * dividendRates[1];
-        double dividend3 = income * dividendRates[2];
-
-        int i = 0;
-        while(i < dividendRates.length){
-            System.out.println("Dividend"+i+" : "+(income*dividendRates[i]));
-        }
+class Accounting{
+    public double valueOfSupply;
+    public double vatRate;
+    public double expenseRate;
+    public void print() {
+        System.out.println("Value of supply : " + valueOfSupply);
+        System.out.println("VAT : " + getVAT());
+        System.out.println("Total : " + getTotal());
+        System.out.println("Expense : " + getExpense());
+        System.out.println("Income : " + getIncome());
+        System.out.println("Dividend 1 : " + getDiviend1());
+        System.out.println("Dividend 2 : " + getDiviend2());
+        System.out.println("Dividend 3 : " + getDiviend3());
     }
-    public static  double getVAT(){
+
+    public double getDiviend1() {
+        return getIncome() * 0.5;
+    }
+    public double getDiviend2() {
+        return getIncome() * 0.3;
+    }
+    public double getDiviend3() {
+        return getIncome() * 0.2;
+    }
+
+    public double getIncome() {
+        return valueOfSupply - getExpense();
+    }
+
+    public double getExpense() {
+        return valueOfSupply * expenseRate;
+    }
+
+    public double getTotal() {
+        return valueOfSupply + getVAT();
+    }
+
+    public double getVAT() {
         return valueOfSupply * vatRate;
     }
+}
+public class AccountingClassApp {
+
+    public static void main(String[] args) {
+        /*Accounting.valueOfSupply = 10000.0;
+        Accounting.vatRate = 0.1;
+        Accounting.expenseRate = 0.3;
+        Accounting.print();
+
+        Accounting.valueOfSupply = 20000.0;
+        Accounting.vatRate = 0.05;
+        Accounting.expenseRate = 0.2;
+        Accounting.print();*/
+
+        Accounting a1 = new Accounting();
+        a1.valueOfSupply = 10000.0;
+        a1.vatRate = 0.1;
+        a1.expenseRate = 0.3;
+        a1.print();
+
+        Accounting a2 = new Accounting();
+        a2.valueOfSupply = 20000.0;
+        a2.vatRate = 0.05;
+        a2.expenseRate = 0.2;
+        a2.print();
+    }
+
+
+
 }
