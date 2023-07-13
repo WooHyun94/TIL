80167c9a63b3a0e8066c4fff2e03f3090eef74a7
WooHyun94 <94dngus@gmail.com>
2023-07-13 10:44:23 +0900

    2023.07.13

---
+++ java_/zero/src/javsStudy/ConstantExam.java
@@ -0,0 +1,18 @@
+package javsStudy;
+
+public class ConstantExam {
+	public static void main(String[] args) {
+		int i;
+		i = 10; 
+		i = 5;
+		
+		final int J;
+		J = 10; 
+		
+		System.out.println(J);
+		
+		final double PI = 3.14159;
+		
+		System.out.println(PI);
+	}
+}
