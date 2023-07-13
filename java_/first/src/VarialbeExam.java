a152ae7de1b33bbcfc01e560009864b42b695885
WooHyun94 <94dngus@gmail.com>
2023-07-13 10:32:46 +0900

    2023.07.13

---
+++ java_/first/src/VarialbeExam.java
@@ -0,0 +1,12 @@
+public class VarialbeExam {
+    public static void main(String[] args) {
+        int count; 
+        
+        count = 10; 
+
+        count = 20;
+
+        System.out.println(count);
+        
+    }
+}
