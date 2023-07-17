97424ad1294652ac42851859770913dfd9c1f407
WooHyun94 <94dngus@gmail.com>
2023-07-17 10:36:13 +0900

    2023.07.17

---
+++ 프로그래머스 문제 풀이/배열 만들기 4.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr) {
+        Stack<Integer> stk = new Stack<>();
+        for (int i = 0; i < arr.length; i++){
+            if (stk.size() == 0){
+                stk.push(arr[i]);
+            }else if (!stk.isEmpty() && stk.peek() < arr[i]){
+                stk.push(arr[i]);
+            }else if (!stk.isEmpty() && stk.peek() >= arr[i]){
+                stk.pop();
+                i--;
+            }
+            
+        }
+        
+        int[] answer = new int[stk.size()];
+        for(int i = stk.size() - 1; i >= 0; i--){
+            answer[i] = stk.pop();
+        }
+        return answer;
+    }
+}
