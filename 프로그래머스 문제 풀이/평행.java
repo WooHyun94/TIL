9a0103372e8b3a50f449035b15379e6b35d6a6bc
WooHyun94 <94dngus@gmail.com>
2023-07-22 18:42:04 +0900

    2023.07.22

---
+++ 프로그래머스 문제 풀이/평행.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] dots) {
+        int answer = 0;
+        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3));
+        for (int i = 1; i < 4; i++){
+            array.remove(Integer.valueOf(i));
+            System.out.println(array.toString());
+            if (((double)(dots[0][0] - dots[i][0]) / (double)(dots[0][1] - dots[i][1]))
+               == ((double)(dots[array.get(0)][0] - dots[array.get(1)][0]) / (double)(dots[array.get(0)][1] - dots[array.get(1)][1]))) return 1;
+            array.add(i);
+        }
+        
+        return answer;
+    }
+}
+
