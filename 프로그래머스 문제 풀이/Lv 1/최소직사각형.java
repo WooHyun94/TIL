3008bbc197bfcc95ee7c697762a7a8b654f7f3fc
WooHyun94 <94dngus@gmail.com>
2023-07-27 10:31:22 +0900

    2023.07.27

---
+++ 프로그래머스 문제 풀이/Lv 1/최소직사각형.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int[] w = new int[sizes.length];
+        int[] h = new int[sizes.length];
+        
+        for (int i = 0; i < sizes.length; i++){
+            if (sizes[i][0] >= sizes[i][1]){
+                w[i] = sizes[i][0];
+                h[i] = sizes[i][1];
+            }else{
+                w[i] = sizes[i][1];
+                h[i] = sizes[i][0];
+            }
+        }
+        Arrays.sort(w);
+        Arrays.sort(h);
+        answer = w[w.length - 1] * h[h.length - 1];
+        return answer;
+    }
+}
+
